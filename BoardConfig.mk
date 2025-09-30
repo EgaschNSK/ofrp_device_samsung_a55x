c38237ff896a630516ecd087ff74d395f5d7dd8b
egaschnsk <egaschnsk@gmail.com>
2025-09-30 20:10:53 +0700

    изменено:      BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,8 +163,6 @@ TW_NO_REBOOT_BOOTLOADER := true
 
 # TWRP Configuration - UI alignment for punch-hole displays
 TW_STATUS_BAR_HEIGHT := 92
-TW_Y_OFFSET := 0
-TW_H_OFFSET := 0
 
 # TWRP Configuration - Version
 TW_DEVICE_VERSION := Lyinceer_04
